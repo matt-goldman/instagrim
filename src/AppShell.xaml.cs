8e56a76ebfb8e69e24cc88c8a2118d207d7674d8
Matt Goldman <matt.goldman@oultook.com>
2026-06-21 18:17:17 +1000

    Added project

---
+++ src/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace instagrim;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace instagrim;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
