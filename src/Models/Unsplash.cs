2d88eedca3e5350b396e867a6b30a9e6413161dd
goldrinco <matt.goldman@arinco.com.au>
2026-06-25 12:45:44 +1000

    Updated unsplash to match actual API results

--- src/Models/Unsplash.cs
+++ src/Models/Unsplash.cs
@@ -6,36 +6,118 @@ public class UnsplashResponse
 {
     public int Total { get; set; }
     public int TotalPages { get; set; }
-    public Results[]? Results { get; set; }
+    public Results[] Results { get; set; } = [];
 }
 
 public class Results
 {
-    public string? Id { get; set; }
-    public string? CreatedAt { get; set; }
+    public required string Id { get; set; }
+    public AlternativeSlugs? AlternativeSlugs { get; set; }
+    public required string CreatedAt { get; set; }
+    public required string UpdatedAt { get; set; }
+    public string? PromotedAt { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
     public string? Color { get; set; }
     public string? BlurHash { get; set; }
-    public string? Description { get; set; }
-    public User? User { get; set; }
+    public required string Description { get; set; }
+    public object[]? Breadcrumbs { get; set; }
+    public required Urls Urls { get; set; }
+    public int Likes { get; set; }
+    public bool LikedByUser { get; set; }
+    public bool Bookmarked { get; set; }
     public object[]? CurrentUserCollections { get; set; }
-    public Urls? Urls { get; set; }
-    public Links? Links { get; set; }
+    public object? Sponsorship { get; set; }
+    public TopicSubmissions? TopicSubmissions { get; set; }
+    public string? AssetType { get; set; }
+    public string? Slug { get; set; }
+    public string? AltDescription { get; set; }
+    public required Links Links { get; set; }
+    public required User User { get; set; }
+}
+
+public class AlternativeSlugs
+{
+    public string? En { get; set; }
+    public string? Es { get; set; }
+    public string? Ja { get; set; }
+    public string? Fr { get; set; }
+    public string? It { get; set; }
+    public string? Ko { get; set; }
+    public string? De { get; set; }
+    public string? Pt { get; set; }
+    public string? Id { get; set; }
+}
+
+public class Urls
+{
+    public required string Raw { get; set; }
+    public required string Full { get; set; }
+    public required string Regular { get; set; }
+    public required string Small { get; set; }
+    public required string Thumb { get; set; }
+    public required string SmallS3 { get; set; }
+}
+
+public class TopicSubmissions
+{
+    public Night? Night { get; set; }
+    public Nature? Nature { get; set; }
+    public Wallpapers? Wallpapers { get; set; }
+}
+
+public class Night
+{
+    public string? Status { get; set; }
+    public string? ApprovedOn { get; set; }
+}
+
+public class Nature
+{
+    public string? Status { get; set; }
+    public string? ApprovedOn { get; set; }
+}
+
+public class Wallpapers
+{
+    public string? Status { get; set; }
+    public string? ApprovedOn { get; set; }
+}
+
+public class Links
+{
+    public required string Self { get; set; }
+    public required string Html { get; set; }
+    public required string Download { get; set; }
+    public required string DownloadLocation { get; set; }
 }
 
 public class User
 {
-    public string? Id { get; set; }
-    public string? Username { get; set; }
+    public required string Id { get; set; }
+    public required string UpdatedAt { get; set; }
+    public required string Username { get; set; }
     public string? Name { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? InstagramUsername { get; set; }
     public string? TwitterUsername { get; set; }
     public string? PortfolioUrl { get; set; }
-    public ProfileImage? ProfileImage { get; set; }
+    public string? Bio { get; set; }
+    public string? Location { get; set; }
     public Links? Links { get; set; }
+    public ProfileImage? ProfileImage { get; set; }
+    public string? InstagramUsername { get; set; }
+    public int TotalCollections { get; set; }
+    public int TotalLikes { get; set; }
+    public int TotalPhotos { get; set; }
+    public int TotalFreePhotos { get; set; }
+    public int TotalPromotedPhotos { get; set; }
+    public int TotalIllustrations { get; set; }
+    public int TotalFreeIllustrations { get; set; }
+    public int TotalPromotedIllustrations { get; set; }
+    public bool AcceptedTos { get; set; }
+    public bool ForHire { get; set; }
+    public Social? Social { get; set; }
 }
 
 public class ProfileImage
@@ -45,27 +127,10 @@ public class ProfileImage
     public string? Large { get; set; }
 }
 
-public class Urls
+public class Social
 {
-    public string? Raw { get; set; }
-    public string? Full { get; set; }
-    public string? Regular { get; set; }
-    public string? Small { get; set; }
-    public string? Thumb { get; set; }
-}
-
-public class Links
-{
-    public string? Self { get; set; }
-    public string? Html { get; set; }
-    public string? Download { get; set; }
-}
-
-// Not converted, parses ApiKey.txt in Resources folder:
-
-public class UnsplashOptions
-{
-    public string? AppId { get; set; }
-    public string? AccessKey { get; set; }
-    public string? SecretKey { get; set; }
+    public string? InstagramUsername { get; set; }
+    public string? PortfolioUrl { get; set; }
+    public string? TwitterUsername { get; set; }
+    public object? PaypalEmail { get; set; }
 }
--- src/Services/ImageService.cs
+++ src/Services/ImageService.cs
@@ -1,4 +1,5 @@
-using System.Text.Json;
+using System.Collections.ObjectModel;
+using System.Net.Http.Json;
 using instagrim.Models;
 
 namespace instagrim.Services;
@@ -8,6 +9,22 @@ public class ImageService
     private HttpClient? _httpClient;
     
     private readonly SemaphoreSlim _httpClientQueue =  new SemaphoreSlim(1, 1);
+
+    private const string SearchQuery = "search/photos?query=halloween";
+
+    private IReadOnlyList<string> Locations =>
+    [
+        "Sleepy Hollow",
+        "Salem",
+        "Transylvania",
+        "Widows Peak",
+        "Arkham Asylum",
+        "Amityville",
+        "Elm Street",
+        "Silent Hill",
+        "Hawkins, Indiana",
+        "Sunnydale"
+    ];
 
     private async Task<HttpClient> GetClient()
     {
@@ -21,18 +38,49 @@ public class ImageService
         await using var stream = await FileSystem.OpenAppPackageFileAsync("ApiKey.txt");
         using var reader = new StreamReader(stream);
 
-        var contents = await reader.ReadToEndAsync();
+        var apiKey = await reader.ReadToEndAsync();
         
-        var options = JsonSerializer.Deserialize<UnsplashOptions>(contents);
 
         _httpClient = new HttpClient
         {
-            BaseAddress = new Uri("https://api.unsplash.com")
+            BaseAddress = new Uri("https://api.unsplash.com/")
         };
         
-        _httpClient.DefaultRequestHeaders.Add("Client-ID", options!.AccessKey);
+        _httpClient.DefaultRequestHeaders.Add("Client-ID", apiKey);
         
         _httpClientQueue.Release();
         return _httpClient;
+    }
+
+    public async Task<List<Post>> GetFeed()
+    {
+        var client = await GetClient();
+        var unsplashResponse = await client.GetFromJsonAsync<UnsplashResponse>(SearchQuery);
+
+        if (!(unsplashResponse?.Results?.Length > 0))
+        {
+            return [];
+        }
+
+        var posts = new List<Post>();
+
+        foreach (var result in unsplashResponse.Results)
+        {
+            var locationIndex = Random.Shared.Next(Locations.Count);
+            var location = Locations[locationIndex];
+            posts.Add(new Post
+            {
+                ImageUrl    = result.Urls!.Regular!,
+                Location    = location,
+                Username    = result.User.InstagramUsername??result.User.Username,
+                Description = result.Description!,
+                Likes       = result.Likes,
+                IsFavourite = Random.Shared.Next() % 2 == 0,
+                Comments    = result.User.TotalPhotos,
+                IsLiked     = Random.Shared.Next() % 2 == 0,
+            });
+        }
+        
+        return posts;
     }
 }
